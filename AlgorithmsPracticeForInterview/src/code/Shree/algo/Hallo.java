873351caab0587ae35a5ccf928617d9ab532e5b9
shreekant10 <shreekant10aug@yahoo.co.in>
2016-02-21 13:43:00 +0530

    1.Hallo.java added author and since.

--- AlgorithmsPracticeForInterview/src/code/Shree/algo/Hallo.java
+++ AlgorithmsPracticeForInterview/src/code/Shree/algo/Hallo.java
@@ -1,5 +1,13 @@
 package code.Shree.algo;
 
-public class Hallo {
 
+/**
+ * 
+ * @author Shree
+ * @since 21feb2016
+ */
+public class Hallo {
+	public static void main(String[] args) {
+		System.out.println("hello world");
+	}
 }
