cd276754c63402de28510f628eefd7f58300d6d5
ravikson <springframeworkbyravi@gmail.com>
2016-02-23 20:35:59 +0530

    alsways declare proper variable name and method, and provide @decription on class level

--- AlgorithmsPracticeForInterview/src/code/Kiran/algo/Sorting_arrays.java
+++ AlgorithmsPracticeForInterview/src/code/Kiran/algo/Sorting_arrays.java
@@ -5,21 +5,25 @@ import java.util.Arrays;
 /**
  * sorting an array.
  * 
- * @author Kiran. an unsorted array is to be sorted here using sort() method of
- *         Arrays class.
+ * @author Kiran
+ * 
+ * @description an unsorted array is to be sorted here using sort() method of
+ *              Arrays class.
  */
 public class Sorting_arrays {
 	public static void main(String[] args) {
-		int[] array = new int[5];
-		array[0] = 19;
-		array[1] = 14;
-		array[2] = 30;
-		array[3] = 1;
-		array[4] = 13;
-		Arrays.sort(array);
 
-		for (int i = 0; i < array.length; i++) {
-			System.out.println(array[i]);
+		int[] intArray = new int[5];
+		intArray[0] = 19;
+		intArray[1] = 14;
+		intArray[2] = 30;
+		intArray[3] = 1;
+		intArray[4] = 13;
+
+		Arrays.sort(intArray);
+
+		for (int i = 0; i < intArray.length; i++) {
+			System.out.println(intArray[i]);
 		}
 	}
 }
