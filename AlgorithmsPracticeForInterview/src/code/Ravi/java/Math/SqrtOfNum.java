4f81ed9040797aef37f7e9d91c579c8f63a52d29
ravi_k_soni <ravikantsoni03@gmail.com>
2016-02-27 13:20:47 +0530

    find sqrt of number using Math class

---
+++ AlgorithmsPracticeForInterview/src/code/Ravi/java/Math/SqrtOfNum.java
@@ -0,0 +1,18 @@
+package code.Ravi.java.Math;
+
+/**
+ * Find sqrt of number using Math class
+ * 
+ * @author ravikson
+ * 
+ */
+public class SqrtOfNum {
+
+	public static void main(String[] args) {
+		//int num = 9;
+		int num = 7;
+		int sqtNum = (int) Math.sqrt(num);
+		System.out.println(sqtNum);
+	}
+
+}
