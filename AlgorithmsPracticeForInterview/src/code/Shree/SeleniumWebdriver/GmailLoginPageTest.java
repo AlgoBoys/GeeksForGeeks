f5004f97ae6e011f9c7b63deaabea59982459e0d
shreekant10 <shreekant10aug@yahoo.co.in>
2016-03-11 18:53:11 +0530

    Gmail login page open & logout .

--- AlgorithmsPracticeForInterview/src/code/Shree/SeleniumWebdriver/GmailLoginPageTest.java
+++ AlgorithmsPracticeForInterview/src/code/Shree/SeleniumWebdriver/GmailLoginPageTest.java
@@ -9,15 +9,15 @@ public class GmailLoginPageTest {
 	public static void main(String[] args) throws InterruptedException {
 		WebDriver driver = new FirefoxDriver();
 		driver.get("http://gmail.com");
-		driver.findElement(By.id("Email")).sendKeys("seleniumocme");
-		driver.findElement(By.id("next")).click();
-		Thread.sleep(2000);
-		driver.findElement(By.id("Passwd")).sendKeys("justenter");
-		driver.findElement(By.id("signIn")).click();
-		Thread.sleep(6000);
-		driver.findElement(By.xpath("//*[@id='gb']/div[1]/div[1]/div[2]/div[4]/div[1]/a/span")).click();
-		Thread.sleep(3000);
-		driver.findElement(By.xpath("//a[text()='Sign out']")).click();
+		 driver.findElement(By.id("Email")).sendKeys("seleniumocme");
+		 driver.findElement(By.id("next")).click();
+		 Thread.sleep(2000);
+		 driver.findElement(By.id("Passwd")).sendKeys("justenter");
+		 driver.findElement(By.id("signIn")).click();
+		 Thread.sleep(6000);
+		 driver.findElement(By.xpath("//span[@class='gb_0a gbii']")).click();
+		 Thread.sleep(3000);
+		 driver.findElement(By.xpath("//a[text()='Sign out']")).click();
 		System.out.println("Hello");
 	}
 
